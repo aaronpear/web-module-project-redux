2adf1b96a4effe7aa4a5f46fe1f1b8f372a5ae36
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2021-12-07 16:32:26 -0800

    Completed add/delete movies

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -10,7 +10,12 @@ const reducer = (state = initialState, action) => {
     switch(action.type) {
         case DELETE_MOVIE:
             return {
-                movies: state.movies.filter(item=>(action.payload !== item.id))
+                movies: state.movies.filter(item=>(parseInt(action.payload) !== item.id))
+            }
+        case ADD_MOVIE:
+            return {
+                ...state,
+                movies: [...state.movies, action.payload]
             }
         default:
             return state;
